96bb1cce428a54afd47d0b168bc1ea0195344132
zhanghc <zhhaochen@gmail.com>
2020-08-31 20:45:05 +0800

    list_node_cycle

---
+++ LeetCode/src/Top100/Linked_List_Cycle.java
@@ -0,0 +1,31 @@
+package Top100;
+
+/**
+ * 2020.08.31
+ * Given a linked list, determine if it has a cycle in it.
+ * To represent a cycle in the given linked list,
+ * we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to.
+ * If pos is -1, then there is no cycle in the linked list.
+ */
+public class Linked_List_Cycle {
+    public boolean hasCycle(ListNode head) {
+        // 快慢指针的典型问题
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+class ListNode{
+    ListNode next;
+    int x;
+    public ListNode(int val){
+        this.x = val;
+    }
+}

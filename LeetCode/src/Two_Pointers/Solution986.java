a8a05086505539aca624727987c75101955a737e
zhanghc <zhhaochen@gmail.com>
2020-10-03 22:45:29 +0800

    interval

--- LeetCode/src/Two_Pointers/Solution986.java
+++ LeetCode/src/Two_Pointers/Solution986.java
@@ -53,4 +53,42 @@ public class Solution986 {
         }
         return res;
     }
+    public int[][] intervalIntersection_simple(int[][] A, int[][] B) {
+        if (A == null || B == null){
+            return new int[0][];
+        }
+        int m = A.length;
+        int n = B.length;
+        if (m == 0 || n == 0){
+            return new int[0][];
+        }
+        int p1 = 0, p2 = 0;
+        List<List<Integer>> res_list = new ArrayList<>();
+        while (p1 < m && p2 < n){
+            int a1 = A[p1][0], a2 = A[p1][1];
+            int b1 = B[p2][0], b2 = B[p2][1];
+            if (b2 < a1 || a2 < b1){
+                // 无交集
+            }else if (b2 >= a1 && a2 >= b1){
+                // 对上面取反即有交集的条件
+                List<Integer> tmp = new ArrayList<>();
+                tmp.add(Math.max(a1, b1));
+                tmp.add(Math.min(a2, b2));
+                res_list.add(tmp);
+            }
+            // 指针移动，只需要判断这两个大小
+            if (a2 > b2){
+                p2++;
+            }else {
+                p1++;
+            }
+        }
+        int[][] res = new int[res_list.size()][2];
+        int index = 0;
+        for(List<Integer> list : res_list){
+            res[index][0] = list.get(0);
+            res[index++][1] = list.get(1);
+        }
+        return res;
+    }
 }
